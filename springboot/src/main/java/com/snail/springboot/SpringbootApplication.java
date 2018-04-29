4002fc11760905fab730337c707f1cca7d5d73ee
snail <xuchuanliang361@163.com>
2018-04-29 15:02:45 +0800

    [test]

--- springboot/src/main/java/com/snail/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/snail/springboot/SpringbootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringbootApplication {
-
+	//测试github是否已经关联上
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
