c46d30103c090de8912b74709c66884ae3ea8658
snail <xuchuanliang361@163.com>
2018-04-29 15:03:26 +0800

    [test]

--- springboot/src/main/java/com/snail/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/snail/springboot/SpringbootApplication.java
@@ -2,11 +2,9 @@ package com.snail.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringbootApplication {
-	//测试github是否已经关联上
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
